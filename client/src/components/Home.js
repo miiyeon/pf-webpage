4aea4068d9700254964b084ae3cc8515a554ab4e
Miyeon Lam <miyeon.lam@web.de>
2023-01-11 11:36:21 +0100

    fetch my data for newsfeed

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,16 +2,28 @@ import { useState, useEffect } from "react";
 
 function Home() {
     const [news, setNews] = useState([]);
+    const [bossData, setBossData] = useState([]);
 
     //fetch news from database
     useEffect(() => {
         fetch("/newsfeed")
             .then((res) => res.json())
             .then((allnews) => {
-                console.log(allnews);
+                // console.log("news: ", allnews);
                 setNews(allnews);
             })
-            .catch((error) => console.log(error));
+            .catch((error) => console.log("error at newsfeed: ", error));
+    }, []);
+
+    //fetch my data from database
+    useEffect(() => {
+        fetch("/boss")
+            .then((res) => res.json())
+            .then((data) => {
+                console.log("boss: ", data);
+                setBossData(data);
+            })
+            .catch((error) => console.log("error at boss fetch: ", error));
     }, []);
 
     return (
@@ -23,7 +35,20 @@ function Home() {
                         console.log(newsentry);
                         return (
                             <div className="entry" key={newsentry.id}>
-                                <section>{newsentry.news}</section>
+                                <section>
+                                    {newsentry.news} {newsentry.created_at}
+                                </section>
+                            </div>
+                        );
+                    })}
+                {bossData &&
+                    bossData.map((data) => {
+                        console.log(data);
+                        return (
+                            <div className="myData" key={data.id}>
+                                <section>
+                                    {data.firstname} {data.lastname}
+                                </section>
                             </div>
                         );
                     })}
