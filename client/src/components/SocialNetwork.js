8fda834b1fefa592981a6d3ec9e199a41095c612
Miyeon Lam <miyeon.lam@web.de>
2023-01-12 13:51:44 +0100

    added instagram page

--- client/src/components/SocialNetwork.js
+++ client/src/components/SocialNetwork.js
@@ -1,5 +1,6 @@
 import "./SocialNetwork.css";
 import { ImFacebook2, ImInstagram, ImWhatsapp } from "react-icons/im";
+import { Link, BrowserRouter } from "react-router-dom";
 
 function SocialNetwork() {
     return (
@@ -7,7 +8,12 @@ function SocialNetwork() {
             <a href="#" target="_blank" className="facebook-icon">
                 <ImFacebook2 size="35px" className="fb-icon" />
             </a>
-            <a href="#" target="_blank" className="instagram-icon">
+            <a
+                href="https://www.instagram.com/cattery_of_precious_fate/"
+                className="instagram-icon"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
                 <ImInstagram size="35px" className="insta-icon" />
             </a>
             <a href="#" target="_blank" className="whatsapp-icon">
